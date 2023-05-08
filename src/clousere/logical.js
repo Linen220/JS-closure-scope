8dd274f8775035232653fa2ad499f3a177d4f67f
Linen220 <fernandorondon2009@hotmail.com>
2023-05-08 00:13:42 -0500

    final commit

---
+++ src/clousere/logical.js
@@ -0,0 +1,24 @@
+// function sumWithClosure(firstNum = 0){
+//     function suma(secondNumb = 0){
+//         return secondNumb + firstNum;
+//     }
+//     return suma;
+// }
+// console.log(sumWithClosure(10)());
+
+// function createPetList() {
+//     let pets = [];
+//     function insertPet(pet) {
+//         if (!pet) {
+//             return pets;
+//         } else{
+//             return pets.push(pet);
+//         }
+//     }
+//     return insertPet;
+//   }
+
+// const myPetList = createPetList();
+
+// console.log(myPetList('10'));
+// console.log(myPetList());
